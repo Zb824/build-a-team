9cdee592fbf798eab2b19db13673a7e37cffa648
Zb824 <zborders28@outlook.com>
2023-01-30 20:05:42 -0600

    final

--- src/htmlgenerator.js
+++ src/htmlgenerator.js
@@ -1,39 +1,68 @@
-function createManagerCard(manager){
+function createManagerCard(manager) {
   return `
-  <h2>${manager.name} </h2>
-  <h2>${manager.id}</h2>
-  <h2>${manager.email}</h2>
-  <h2>${manager.officeNumber}</h2>
+  <div class="card employee" style="width: 30rem;">
+  <div class="card-header">
+    Manger
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Name: ${manager.getName()} </li>
+    <li class="list-group-item">Id Number: ${manager.getId()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+    <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+  </ul>
+</div>
+
+
+
+
   `
 }
-function createEngineerCard(engineer){
+function createEngineerCard(engineer) {
   return `
-  <h2>${engineer.name} </h2>
-  <h2>${engineer.name} </h2>
-  <h2>${engineer.name} </h2>
-  <h2>${engineer.name} </h2>
+
+  <div class="card employee" style="width: 30rem;">
+  <div class="card-header">
+    Engineer
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Name: ${engineer.getName()} </li>
+    <li class="list-group-item">Id Number: ${engineer.getId()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+   <li class="list-group-item">Github: <a class="card-link" href="https://github.com/${engineer.getGithub()}"> ${engineer.getGithub()}</a></li>
+  </ul>
+</div>
+
+  
   `
 }
-function createInternCard(intern){
+function createInternCard(intern) {
   return `
-  <h2>${intern.name} </h2>
-  <h2>${intern.name} </h2>
-  <h2>${intern.name} </h2>
-  <h2>${intern.name} </h2>
+  <div class="card employee" style="width: 30rem;">
+  <div class="card-header">
+    Intern
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Name: ${intern.getName()} </li>
+    <li class="list-group-item">Id Number: ${intern.getId()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+    <li class="list-group-item">School: ${intern.getSchool()}</li>
+  </ul>
+</div>
+
   `
 }
 
-function createCards(team){
+function createCards(team) {
   const cardArray = []
-  cardArray.push(team.filter(employee => employee.getRole()=== "Manager").map(manager => createManagerCard(manager)).join(""))
-  cardArray.push(team.filter(employee => employee.getRole()=== "Engineer").map(engineer => createEngineerCard(engineer)).join(""))
-  cardArray.push(team.filter(employee => employee.getRole()=== "Intern").map(intern => createInternCard(intern)).join(""))
+  cardArray.push(team.filter(employee => employee.getRole() === "Manager").map(manager => createManagerCard(manager)).join(""))
+  cardArray.push(team.filter(employee => employee.getRole() === "Engineer").map(engineer => createEngineerCard(engineer)).join(""))
+  cardArray.push(team.filter(employee => employee.getRole() === "Intern").map(intern => createInternCard(intern)).join(""))
 
   return cardArray.join("")
 
 }
 
-module.exports = function(team){
+module.exports = function (team) {
   return `
   <!DOCTYPE html>
 <html lang="en">
@@ -41,18 +70,27 @@ module.exports = function(team){
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>New Team</title>
-</head>
-<body>
-     <div class ="container-fluid">
-        <div class = "row">
-            <div class ="col-12">
-${createCards(team)}
-            </div>
-        </div>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <article>
+    <header>
+      <h1 class ="text-center"> My Team </h1>
+    </header>
+  </article>
+  <body>
+   <div class ="container-fluid">
+      <div class = "row">
+      <div class="d-flex flex-wrap items">
+  
+  ${createCards(team)} 
+  
+    </div>
+    </div>
     </div> 
   
-</body>
+  
+  
+  </body>
 </html>
   
   `
